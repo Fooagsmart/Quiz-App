39400a2e7ba550f44ceba3d42073e0f3fc0d6ee4
Oluwaseun <adunmofestus@gmail.com>
2023-09-26 21:53:16 +0100

    const.js

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+     question: "Which is the largest animal in the world",
+     answers:[
+        {text: "shark", correct: false},
+        {text: "Blue Whale", correct: true},
+        {text: "Elephant", correct: false},
+        {text: "Giraffe", correct: false},
+     ]   
+    },
+    {
+        question: "Which of this is not protein",
+        answers:[
+           {text: "moi-moi", correct: false},
+           {text: "rice", correct: true},
+           {text: "egg", correct: false},
+           {text: "beans", correct: false},
+        ]   
+       },
+       {
+        question: "Which is the largest desert in the world",
+        answers:[
+           {text: "Kalahari", correct: false},
+           {text: "Gobi", correct: false},
+           {text: "sahara", correct: false},
+           {text: "Antarctica", correct: true},
+        ]   
+       },
+       {
+        question: "Which is the smallest continent in the world",
+        answers:[
+           {text: "Asia", correct: false},
+           {text: "Australia", correct: true},
+           {text: "Arctic", correct: false},
+           {text: "Africa", correct: false},
+        ]   
+       }
+
+];

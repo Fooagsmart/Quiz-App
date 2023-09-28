f46571bb1087c810bf461e80760a00d4ed39f679
Oluwaseun <adunmofestus@gmail.com>
2023-09-28 03:46:22 +0100

    show score function

--- script.js
+++ script.js
@@ -86,6 +86,7 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++; 
     }else{
         selectedBtn.classList.add("incorrect");
     }
@@ -99,6 +100,28 @@ function selectAnswer(e){
 
 }
 
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
 
+function  handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
 
 startQuiz();
